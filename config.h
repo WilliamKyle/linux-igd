667fc110081fa9bbf60dce1fee8df1a65dabcb31
andbe611 <>
2003-09-24 10:22:53 +0000

    Moved the global vars: extInterfaceName, intInterfaceName, intIpAddress[16] to file globals.h Added def for function parseConfigFile

--- config.h
+++ config.h
@@ -1,8 +1,9 @@
 #ifndef _CONFIG_H_
 	#define _CONFIG_H_
 
-char extInterfaceName[10];
-char intInterfaceName[10];
-char intIpAddress[16];
+int parseConfigFile(int *insert_forward_rules, int *debug_mode, char iptables_location[],
+		    char forward_chain_name[], char prerouting_chain_name[],
+		    char upstream_bitrate[], char downstream_bitrate[],
+		    char desc_doc[], char xml_path[]);
 
 #endif // _CONFIG_H_
